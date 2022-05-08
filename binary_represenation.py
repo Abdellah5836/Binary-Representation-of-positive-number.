7861a1941f5f0ea6a8b1fcebf6f1f1ddf2633aeb
Abdo.Ch99 <60854648+Abdellah5836@users.noreply.github.com>
2022-05-08 00:34:33 +0000

    Update binary_represenation.py

--- binary_represenation.py
+++ binary_represenation.py
@@ -11,42 +11,38 @@ List of remainders: 0, 1, 1.
 Reversed is 1, 1, 0, which is also the binary representation of 6.
 '''
 
-from sympy import rem
 
+from text_colors import TextColors
 
-remainders_dict = []
-result_list = []
-def Binary_num(remainders_dict, result_list):
-    """
-    Baniary representation of a positive number
-    """
-    query = input("Enter a Positive Number: ")
-    if int(query) < 0:
-        print("**Please, Enter a POSITIVE Number.**")
-        return 0
-    pos_num = int(query)
-    while pos_num > 0:
-        result_list.append(pos_num)
-        remainder = pos_num % 2
-        remainders_dict.append(remainder)
-        pos_num //= 2
+tc = TextColors
 
+def binary_rep():
+    list_remainders = []
 
+    query = input(f"{tc.magenta}[+]Enter a positive number:{tc.reset} ")
+    num = int(query)
+    if num < 0:
+        return f"{tc.red}[!]Unrecognized number!\n{tc.reset}"
 
-def display_binay_num(remainders_dict, result_list):
-    """
-    Display remainders list content
-    """
-    print(f"List of remainders: {remainders_dict}")
-    print("~~" * 20)
-    print(f"Reversed is {remainders_dict[::-1]},"
-        f" which is also the binary representation of {result_list[0]}.")
+    while num > 0:
+        binary = num % 2
+        list_remainders.append(binary)
+        num //= 2
+        
+    print(f"\n{tc.green}[~]List of remainders:{tc.reset} {list_remainders}")
+    list_remainders.reverse()
+    print(f"\n{tc.green}[~]Reserved is:{tc.reset} {list_remainders}")
 
+    int_num = len(list_remainders)
+    list_binary = [ 2 ** item for item in range(int_num) ]
+    list_binary.reverse()
+    #print(list_binary)
 
+    product = [list_remainders[i] * list_binary[i] for i in range(len(list_remainders))]
+    #print(product)
+    result = sum(product)
+    #print(f"\n{tc.cyan}Which is also the binary representation of{tc.blue} {result}.{tc.reset}")
+    return f"\n{tc.cyan}[√]Which is also the binary representation of{tc.blue} {result}.{tc.reset}"
 
-print("~~" * 20)
-Binary_num(remainders_dict, result_list)
-print("~~" * 20)
-print(f"Numberes Calculated:\n\t{result_list}")
-print("~~" * 20)
-display_binay_num(remainders_dict, result_list)
+res = binary_rep()
+print(res)
